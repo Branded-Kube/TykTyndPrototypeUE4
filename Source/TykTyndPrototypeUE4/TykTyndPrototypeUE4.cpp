8dafaeb58221456b6a2a5f63b01da208f212431c
mrkoldbrandt <mrkoldbrandt@gmail.com>
2022-04-21 13:45:04 +0200

    Visual studio

---
+++ Source/TykTyndPrototypeUE4/TykTyndPrototypeUE4.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TykTyndPrototypeUE4.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TykTyndPrototypeUE4, "TykTyndPrototypeUE4" );
